8f5e7f7d8c6e1adbb5d7d624c1d0b90882e42c65
Dervis B Uludag <55367496+blackpars4x4@users.noreply.github.com>
2020-11-17 21:46:44 -0800

    Nov17-3

--- src/day30_ReturnMethods/MethodCalls.java
+++ src/day30_ReturnMethods/MethodCalls.java
@@ -24,8 +24,6 @@ public class MethodCalls {
         System.out.println(result3);
 
 
-
-
     }
 
 
--- src/library/StringUtility.java
+++ src/library/StringUtility.java
@@ -30,10 +30,10 @@ public class StringUtility {
         return unique;
     }
 
-    public static String reverse(String str){
+    public static String reverse(String str) {
         String result = "";
 
-        for(int i = str.length()-1; i >= 0; i--){
+        for (int i = str.length() - 1; i >= 0; i--) {
             result += str.charAt(i);
         }
 
