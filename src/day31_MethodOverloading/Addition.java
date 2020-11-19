8e9d9f0b3f94008934c205fabb57846d939d36bc
Dervis <blackpars4x4@gmail.com>
2020-11-19 10:22:42 -0600

    Day31-11/19/20

--- src/day31_MethodOverloading/Addition.java
+++ src/day31_MethodOverloading/Addition.java
@@ -8,9 +8,11 @@ public class Addition {
     public static int additionOf2Numbers(int a, int b){
         return a+b;
     }
+
     public static int additionOf3Numbers(int a, int b, int c){
         return additionOf2Numbers(a, b) + c;
     }
+
     public static int additionOf4Numbers(int a, int b, int c, int d){
         return additionOf2Numbers(a, b) + additionOf2Numbers(c, d);
     }
